799c5557e725bbee7204384c5538ad0269eb974c
zzinLee <sujin951017@gmail.com>
2024-10-10 11:26:07 +0900

    🤡 Add init mock

--- src/ui/components/Editor/const.ts
+++ src/ui/components/Editor/const.ts
@@ -1,14 +1,65 @@
 export const INIT_DATA = {
-  time: new Date().getTime(),
-  blocks: [
-    {
-      type: "header",
-      data: {
-        text: "this is editor!",
-        level: 1,
+  "time": 1550476186479,
+  "blocks": [
+      {
+        "id": "oUq2g_tl8y",
+        "type": "header",
+        "data": {
+            "text": "Editor.js",
+            "level": 2
+        }
       },
+      {
+        "id": "zbGZFPM-iI",
+        "type": "paragraph",
+        "data": {
+            "text": "Hey. Meet the new Editor. On this page you can see it in action — try to edit this text. Source code of the page contains the example of connection and configuration."
+        }
+      },
+      {
+        "id": "qYIGsjS5rt",
+        "type": "header",
+        "data": {
+            "text": "Key features",
+            "level": 3
+        }
+      },
+      {
+        "id": "XV87kJS_H1",
+        "type": "list",
+        "data": {
+            "style": "unordered",
+            "items": [
+              "It is a block-styled editor",
+              "It returns clean data output in JSON",
+              "Designed to be extendable and pluggable with a simple API"
+            ]
+        }
+      },
+      {
+        "id": "AOulAjL8XM",
+        "type": "header",
+        "data": {
+            "text": "What does it mean «block-styled editor»",
+            "level": 3
+        }
+      },
+      {
+        "id": "cyZjplMOZ0",
+        "type": "paragraph",
+        "data": {
+            "text": "Workspace in classic editors is made of a single contenteditable element, used to create different HTML markups. Editor.js <mark class=\"cdx-marker\">workspace consists of separate Blocks: paragraphs, headings, images, lists, quotes, etc</mark>. Each of them is an independent contenteditable element (or more complex structure) provided by Plugin and united by Editor's Core."
+        }
+    },
+    {
+      id: "mock_Id",
+      type: "image",
+      data: {
+        url: "https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_1280.jpg"
+      }
     },
   ],
+  "version": "2.8.1"
 };
 
 export const EDIT_DIV_ID = "student-feedback";
